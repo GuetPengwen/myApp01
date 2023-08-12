8ce27771eebe1ea8b1bbae6cf7aa863502ea4e9e
mrx <2835323498@qq.com>
2023-08-12 17:52:57 +0800

    整合了商品列表（商品分类）和商品详情和首页

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -23,65 +23,64 @@ export default class Index extends Component {
 
   render(){
 
-  return (
-    <View className='index'>
-      <Swiper
-        className='swiper'
-        vertical={false}
-        circular
-        autoplay
-      >
-        <SwiperItem className='swiper-item'>
-          <image src='https://g1.juntaitec.cn/images/3.jpg'></image>
-        </SwiperItem>
-        <SwiperItem className='swiper-item'>
-          <image src='https://g1.juntaitec.cn/images/102.jpg'></image>
-        </SwiperItem>
-        <SwiperItem className='swiper-item'>
-          <image src='https://g1.juntaitec.cn/images/101.jpg'></image>
-        </SwiperItem>
-      </Swiper>
+    return (
+      <View className='index'>
+        <Swiper
+          className='swiper'
+          vertical={false}
+          circular
+          autoplay
+        >
+          <SwiperItem className='swiper-item'>
+            <image src='https://g1.juntaitec.cn/images/3.jpg'></image>
+          </SwiperItem>
+          <SwiperItem className='swiper-item'>
+            <image src='https://g1.juntaitec.cn/images/102.jpg'></image>
+          </SwiperItem>
+          <SwiperItem className='swiper-item'>
+            <image src='https://g1.juntaitec.cn/images/101.jpg'></image>
+          </SwiperItem>
+        </Swiper>
 
-      <Button className='btn1' type='default'>
-        茶C社群获取【专属优惠券口令】
-        <text className='bte1'>-输入口令-</text>
-      </Button>
-      <Button className='btn2' type='default'>
-        <text className='bte2-1'>点亮计划</text>
-        <text className='bte2-2'>集满5单即可解锁半价券</text>
-        <AtRate
-          size='30'
-          margin='50'
-          value={this.state.value}
+        <Button className='btn1' type='default'>
+          茶C社群获取【专属优惠券口令】
+          <text className='bte1'>-输入口令-</text>
+        </Button>
+        <Button className='btn2' type='default'>
+          <text className='bte2-1'>点亮计划</text>
+          <text className='bte2-2'>集满5单即可解锁半价券</text>
+          <AtRate
+            size='30'
+            margin='50'
+            value={this.state.value}
 
-        />
-      </Button>
-      <View className='BTN'>
-        <Button className='btn3-1'>
-          <image class='in' src='https://g1.juntaitec.cn/images/index3-3.jpg' onClick={this.jumpToOrder}></image>
+          />
         </Button>
-        <Button className='btn3-2' >
-          <image class='out' src='https://g1.juntaitec.cn/images/index3-2.jpg' onClick={this.jumpToOrder}></image>
-        </Button>
+        <View className='BTN'>
+          <Button className='btn3-1'>
+            <image class='in' src='https://g1.juntaitec.cn/images/index3-3.jpg' onClick={this.jumpToOrder}></image>
+          </Button>
+          <Button className='btn3-2' >
+            <image class='out' src='https://g1.juntaitec.cn/images/index3-2.jpg' onClick={this.jumpToOrder}></image>
+          </Button>
+        </View>
+
+        <View className='BTN2'>
+          <Button className='btn4-1'>
+            <image class='tu' src='https://g1.juntaitec.cn/images/index4-1.jpg' onClick={this.jumpToOrder}></image>
+          </Button>
+          <Button className='btn4-2' >
+            <image class='tu' src='https://g1.juntaitec.cn/images/index4-2.jpg' onClick={this.jumpToOrder}></image>
+          </Button>
+          <Button className='btn4-3'>
+            <image class='tu' src='https://g1.juntaitec.cn/images/index4-3.jpg' onClick={this.jumpToOrder}></image>
+          </Button>
+          <Button className='btn4-4' >
+            <image class='tu' src='https://g1.juntaitec.cn/images/index4-4.jpg' onClick={this.jumpToOrder}></image>
+          </Button>
+        </View>
+        <TabBar current={0}></TabBar>
       </View>
-
-      <View className='BTN2'>
-        <Button className='btn4-1'>
-          <image class='tu' src='https://g1.juntaitec.cn/images/index4-1.jpg' onClick={this.jumpToOrder}></image>
-        </Button>
-        <Button className='btn4-2' >
-          <image class='tu' src='https://g1.juntaitec.cn/images/index4-2.jpg' onClick={this.jumpToOrder}></image>
-        </Button>
-        <Button className='btn4-3'>
-          <image class='tu' src='https://g1.juntaitec.cn/images/index4-3.jpg' onClick={this.jumpToOrder}></image>
-        </Button>
-        <Button className='btn4-4' >
-          <image class='tu' src='https://g1.juntaitec.cn/images/index4-2.jpg' onClick={this.jumpToOrder}></image>
-        </Button>
-      </View>
-      <TabBar current={0}></TabBar>
-    </View>
-   )
- }
+    )
+  }
 }
-
