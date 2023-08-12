fc02f9f086be8a2ed2005c346f37748ddc4c4b55
mrx <2835323498@qq.com>
2023-08-12 16:25:53 +0800

    整合了商品列表（商品分类）和商品详情

--- src/pages/product-detail/index.jsx
+++ src/pages/product-detail/index.jsx
@@ -2,13 +2,12 @@ import {Component} from "react";
 import {View, Swiper, SwiperItem, Image, Text} from '@tarojs/components'
 import Taro from "@tarojs/taro";
 import './index.scss'
-import TabBar from '../common/Index'
-import { AtTabs, AtTabsPane } from 'taro-ui'
+import {AtTag} from 'taro-ui'
 
 export default class Index extends Component {
 
 
-  componentWillMount(){
+  componentWillMount() {
     // console.log("参数值：",getCurrentInstance().router.params.id);
     //console.log("从主页传递过来的商品详情数据：",Taro.getCurrentInstance().preloadData);
     this.setState({
@@ -16,87 +15,220 @@ export default class Index extends Component {
     })
   }
 
-
-  constructor () {
+  constructor() {
     super(...arguments)
     this.state = {
-      current: 0,
-      product: {},
-      num: 0,
-      showDot:false
+      count:1,
+      activeTags:[],
+      price:11,
+      totalPrice:0
     }
   }
-  handleClick (value) {
-    this.setState({
-      current: value
-    })
-  }
-  addcart(){
-    this.state.num++;
-    if(this.state.num!=0){
-      this.setState({
-        showDot:true
-      })
-    }
-  }
-  showCart(){
-    console.log("查看购物车");
-    Taro.reLaunch({
-      url: '/pages/cart/index'})
 
+  handleAddClick = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+      totalPrice: (prevState.count + 1) * prevState.price,
+    }));
+  };
+
+  handleMinusClick = () => {
+    if (this.state.count > 0) {
+      this.setState((prevState) => ({
+        count: prevState.count - 1,
+        totalPrice: (prevState.count - 1) * prevState.price,
+      }));
+    }
+  };
+
+
+  onClick(tagName) {
+    this.setState((prevState) => {
+      const activeTags = [...prevState.activeTags];
+      const tagIndex = activeTags.indexOf(tagName);
+
+      if (tagIndex === -1) {
+        activeTags.push(tagName);
+      } else {
+        activeTags.splice(tagIndex, 1);
+      }
+
+      return { activeTags };
+    });
   }
+
+  addCart() {
+    console.log('所选标签:', this.state.activeTags);
+    console.log("count>>>",this.state.count);
+    console.log('总价:', this.state.totalPrice);
+  }
+
+
   render() {
-    const tabList = [{ title: '商品详情' }, { title: '产品参数' }]
-    const tempProduct = this.state.product;
-
     return (
-      <View className='product-detial'>
-        <Swiper
-          className='swiper'
-          indicatorColor='#999'
-          indicatorActiveColor='#333'
-          vertical={false}
-          circular
-          indicatorDots
-          autoplay
-        >
-          <SwiperItem className='swiper-item'>
-            <image src={tempProduct.imageOne}></image>
-          </SwiperItem>
-          <SwiperItem className='swiper-item'>
-            <image src={tempProduct.imageTwo}></image>
-          </SwiperItem>
-          <SwiperItem className='swiper-item'>
-            <image src={tempProduct.imageThree}></image>
-          </SwiperItem>
-        </Swiper>
-        {/*商品标题价格*/}
-        <View className='box-demo'>
-          <Text className='title'>{tempProduct.title}{tempProduct.secondtitle}</Text>
-          <Text className='price'>￥{tempProduct.price}</Text>
+      <View className='product-detail'>
+        <View className='head'>
+          <image src='https://img2.baidu.com/it/u=3483381524,4210975000&fm=253&fmt=auto&app=120&f=JPEG?w=661&h=500'></image>
         </View>
-        {/* <!--商品详情展示-->*/}
-        <AtTabs current={this.state.current} tabList={tabList} onClick={this.handleClick.bind(this)}>
-          <AtTabsPane current={this.state.current} index={0} >
-            <Image mode='widthFix' src={tempProduct.productDetail}></Image>
-          </AtTabsPane>
-          <AtTabsPane current={this.state.current} index={1}>
-            <Image mode='widthFix' src={tempProduct.productParameter}></Image>
-          </AtTabsPane>
-        </AtTabs>
-         {/*// <!--底部加入购物车-->*/}
-        <View className='bottom'>
-          <View className='left'>
-            <Image src='http://43.139.94.243/images/jindian.png' style='height:80rpx;width:90rpx'></Image>
-            <Image src='http://43.139.94.243/images/cart.png' style="height:90rpx;width:90rpx" onClick={this.showCart.bind(this)}></Image>
-            {this.state.showDot && <Text className='carts-icon-num'>{this.state.num}</Text>}
 
+        <View className='secondName'>
+          <Text className='text-one'>暴打西瓜波波</Text>
+          <Text className='text-two'>【巨清爽】无仔西瓜+有机绿茶，可冰沙可水果茶</Text>
+        </View>
+
+        <View className='line'></View>
+
+        <View className='choice'>
+
+          <View className='temperature'>
+            <Text>温度</Text>
+            <View className='tem-button'>
+              <AtTag
+                name='沙冰'
+                type='primary'
+                circle
+                active={this.state.activeTags.includes('沙冰')}
+                onClick={() => this.onClick('沙冰')}
+              >
+                沙冰
+              </AtTag>
+              <AtTag
+                name='常规冰'
+                type='primary'
+                circle
+                active={this.state.activeTags.includes('常规冰')}
+                onClick={() => this.onClick('常规冰')}
+              >
+                常规冰
+              </AtTag>
+              <AtTag
+                name='水果茶'
+                type='primary'
+                circle
+                active={this.state.activeTags.includes('水果茶')}
+                onClick={() => this.onClick('水果茶')}
+              >
+                水果茶
+              </AtTag>
+            </View>
           </View>
-          <View className='right'>
-            <Text className='textone' onClick={this.addcart.bind(this)}>加入购物车</Text>
+
+          <View className='sweetness'>
+            <Text>甜度</Text>
+            <View className='sweet-button'>
+              <AtTag
+                name='常规糖'
+                type='primary'
+                circle
+                active={this.state.activeTags.includes('常规糖')}
+                onClick={() => this.onClick('常规糖')}
+              >
+                常规糖
+              </AtTag>
+              <AtTag
+                name='7分糖'
+                type='primary'
+                circle
+                active={this.state.activeTags.includes('7分糖')}
+                onClick={() => this.onClick('7分糖')}
+              >
+                7分糖
+              </AtTag>
+              <AtTag
+                name='5分糖'
+                type='primary'
+                circle
+                active={this.state.activeTags.includes('5分糖')}
+                onClick={() => this.onClick('5分糖')}
+              >
+                5分糖
+              </AtTag>
+            </View>
           </View>
-          <View className='right'>
-            <Text className='texttwo'>联系客服</Text>
+          <View className='material'>
+            <Text>加料</Text>
+            <View className='material-button'>
+              <AtTag
+                name='椰果'
+                type='primary'
+                circle
+                active={this.state.activeTags.includes('椰果')}
+                onClick={() => this.onClick('椰果')}
+              >
+                椰果
+              </AtTag>
+              <AtTag
+                name='寒天'
+                type='primary'
+                circle
+                active={this.state.activeTags.includes('寒天')}
+                onClick={() => this.onClick('寒天')}
+              >
+                寒天
+              </AtTag>
+              <AtTag
+                name='芋圆'
+                type='primary'
+                circle
+                active={this.state.activeTags.includes('芋圆')}
+                onClick={() => this.onClick('芋圆')}
+              >
+                芋圆
+              </AtTag>
+            </View>
+          </View>
+        </View>
+
+        <View className='line'></View>
+
+        <View className='detail'>
+          <Text className='text-one'>商品详情</Text>
+          <Text className='text-three'>暂无详情</Text>
+        </View>
+
+        <View className='grey-block'></View>
+
+
+        {/*// <!--底部加入购物车-->*/}
+        <View className='bottom'>
+          <View className='up'>
+            <View className='up-left'>
+              <View className='price'>
+                <View className='price-left'>￥11.00</View>
+                <View className='vip'>
+                  <View className='vip-left'>vip</View>
+                  <View className='vip-right'>￥10.45</View>
+                </View>
+              </View>
+              <View className='in-text'>常规冰+常规糖</View>
+            </View>
+
+            <View className='up-right'>
+              <View className='minus-circle' onClick={this.handleMinusClick}>
+                <View className='minus'></View>
+              </View>
+              <View className='num'>{this.state.count}</View>
+              <View className='add-circle' onClick={this.handleAddClick}>
+                <View className='add-horizontal'></View>
+                <View className='add-vertical'></View>
+              </View>
+            </View>
+          </View>
+
+          <View className='down'>
+            <View className='left'>
+              <View className='at-icon at-icon-external-link'>
+                <View className='share'>分享</View>
+              </View>
+              <View className='vertical-line'></View>
+              <View className='at-icon at-icon-shopping-cart'>
+                <View className='shoppingCart'>购物车</View>
+              </View>
+            </View>
+
+            <View className='right'>
+              <View className='button' onClick={this.addCart.bind(this)}>加入购物车</View>
+            </View>
           </View>
         </View>
 
