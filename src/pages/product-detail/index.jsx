a34af81a19610005f39d41e299c9ef9c9eae7a5f
刘耀辉book <1031368023@qq.com>
2023-08-12 11:52:23 +0800

    完成了商品详情页面的效果

--- src/pages/product-detail/index.jsx
+++ src/pages/product-detail/index.jsx
@@ -18,34 +18,57 @@ export default class Index extends Component {
   constructor() {
     super(...arguments)
     this.state = {
-      current: 0,
-      product: {},
-      num: 0,
-      showDot: false
+      count:1,
+      activeTags:[],
+      price:11,
+      totalPrice:0
     }
   }
 
-  onClick() {
-    console.log("选中");
+  handleAddClick = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+      totalPrice: (prevState.count + 1) * prevState.price,
+    }));
+  };
+
+  handleMinusClick = () => {
+    if (this.state.count > 0) {
+      this.setState((prevState) => ({
+        count: prevState.count - 1,
+        totalPrice: (prevState.count - 1) * prevState.price,
+      }));
+    }
+  };
+
+
+  onClick(tagName) {
+    this.setState((prevState) => {
+      const activeTags = [...prevState.activeTags];
+      const tagIndex = activeTags.indexOf(tagName);
+
+      if (tagIndex === -1) {
+        activeTags.push(tagName);
+      } else {
+        activeTags.splice(tagIndex, 1);
+      }
+
+      return { activeTags };
+    });
   }
 
-  addcart() {
-    this.state.num++;
-    if (this.state.num != 0) {
-      this.setState({
-        showDot: true
-      })
-    }
+  addCart() {
+    console.log('所选标签:', this.state.activeTags);
+    console.log("count>>>",this.state.count);
+    console.log('总价:', this.state.totalPrice);
   }
 
 
   render() {
-    const tempProduct = this.state.product;
-
     return (
       <View className='product-detail'>
         <View className='head'>
-          <image src='http://g1.juntaitec.cn/images/101.jpg'></image>
+          <image src='https://img2.baidu.com/it/u=3483381524,4210975000&fm=253&fmt=auto&app=120&f=JPEG?w=661&h=500'></image>
         </View>
 
         <View className='secondName'>
@@ -61,26 +84,29 @@ export default class Index extends Component {
             <Text>温度</Text>
             <View className='tem-button'>
               <AtTag
-                name='tag-1'
+                name='沙冰'
                 type='primary'
                 circle
-                onClick={this.onClick.bind(this)}
+                active={this.state.activeTags.includes('沙冰')}
+                onClick={() => this.onClick('沙冰')}
               >
                 沙冰
               </AtTag>
               <AtTag
-                name='tag-1'
+                name='常规冰'
                 type='primary'
                 circle
-                onClick={this.onClick.bind(this)}
+                active={this.state.activeTags.includes('常规冰')}
+                onClick={() => this.onClick('常规冰')}
               >
                 常规冰
               </AtTag>
               <AtTag
-                name='tag-2'
+                name='水果茶'
                 type='primary'
                 circle
-                onClick={this.onClick.bind(this)}
+                active={this.state.activeTags.includes('水果茶')}
+                onClick={() => this.onClick('水果茶')}
               >
                 水果茶
               </AtTag>
@@ -91,28 +117,31 @@ export default class Index extends Component {
             <Text>甜度</Text>
             <View className='sweet-button'>
               <AtTag
-                name='tag-1'
+                name='常规糖'
                 type='primary'
                 circle
-                onClick={this.onClick.bind(this)}
+                active={this.state.activeTags.includes('常规糖')}
+                onClick={() => this.onClick('常规糖')}
               >
                 常规糖
               </AtTag>
               <AtTag
-                name='tag-1'
+                name='7分糖'
                 type='primary'
                 circle
-                onClick={this.onClick.bind(this)}
+                active={this.state.activeTags.includes('7分糖')}
+                onClick={() => this.onClick('7分糖')}
               >
                 7分糖
               </AtTag>
               <AtTag
-                name='tag-1'
+                name='5分糖'
                 type='primary'
                 circle
-                onClick={this.onClick.bind(this)}
+                active={this.state.activeTags.includes('5分糖')}
+                onClick={() => this.onClick('5分糖')}
               >
-                五分糖
+                5分糖
               </AtTag>
             </View>
           </View>
@@ -120,26 +149,29 @@ export default class Index extends Component {
             <Text>加料</Text>
             <View className='material-button'>
               <AtTag
-                name='tag-1'
+                name='椰果'
                 type='primary'
                 circle
-                onClick={this.onClick.bind(this)}
+                active={this.state.activeTags.includes('椰果')}
+                onClick={() => this.onClick('椰果')}
               >
                 椰果
               </AtTag>
               <AtTag
-                name='tag-1'
+                name='寒天'
                 type='primary'
                 circle
-                onClick={this.onClick.bind(this)}
+                active={this.state.activeTags.includes('寒天')}
+                onClick={() => this.onClick('寒天')}
               >
                 寒天
               </AtTag>
               <AtTag
-                name='tag-1'
+                name='芋圆'
                 type='primary'
                 circle
-                onClick={this.onClick.bind(this)}
+                active={this.state.activeTags.includes('芋圆')}
+                onClick={() => this.onClick('芋圆')}
               >
                 芋圆
               </AtTag>
@@ -172,11 +204,11 @@ export default class Index extends Component {
             </View>
 
             <View className='up-right'>
-              <View className='minus-circle'>
+              <View className='minus-circle' onClick={this.handleMinusClick}>
                 <View className='minus'></View>
               </View>
-              <View className='num'>0</View>
-              <View className='add-circle'>
+              <View className='num'>{this.state.count}</View>
+              <View className='add-circle' onClick={this.handleAddClick}>
                 <View className='add-horizontal'></View>
                 <View className='add-vertical'></View>
               </View>
@@ -195,7 +227,7 @@ export default class Index extends Component {
             </View>
 
             <View className='right'>
-              <View className='button' onClick={this.addcart.bind(this)}>加入购物车</View>
+              <View className='button' onClick={this.addCart.bind(this)}>加入购物车</View>
             </View>
           </View>
         </View>
